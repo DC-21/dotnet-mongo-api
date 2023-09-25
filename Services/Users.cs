191ea48b081fc9b96eb8a88bd0d50b2db56365b3
DC-21 <cholahkuboko@gmail.com>
2023-09-25 13:44:52 +0200

    first commit

--- Services/Users.cs
+++ Services/Users.cs
@@ -1,11 +1,10 @@
-using MySql.Data;
+
 using CrudApi.Models;
 using CrudApi.Configurations;
 using Microsoft.Extensions.Options;
 
 namespace CrudApi.Services;
-
 public class UsersService
 {
-    public async Task<List<User>>
+    public async Task<List<User>> GetUsersAsync()=> await Users.Find(_=>true)
 }
